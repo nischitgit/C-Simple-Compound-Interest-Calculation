3e5b712c3ba6f4dba7d40ec6941f3cf9b5b9f2c7
Nischit <123927039+NischitGit@users.noreply.github.com>
2023-01-30 12:37:13 +0530

    Create compound_interest.c

---
+++ compound_interest.c
@@ -0,0 +1,22 @@
+// Compound Interest Calculaiton
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  // Variables
+  int principal, rate, time, compound_interest;
+  // Instructions
+  printf("Compound Interest Calculaiton \n");
+  printf("Enter Principal Amount: ");
+  scanf("%d", &principal);
+  printf("Enter Rate of Interest: ");
+  scanf("%d", &rate);
+  printf("Enter Time Period: ");
+  scanf("%d", &time);
+  // Output
+  compound_interest = principal * (pow((1 + rate / time), time));
+  printf("Principal Amount = %d \n", principal);
+  printf("Total Interest = %d \n", compound_interest);
+  printf("Total Amount = %d", principal + compound_interest);
+  return 0;
+}
