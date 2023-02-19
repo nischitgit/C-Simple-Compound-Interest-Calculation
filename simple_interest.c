6427e4be5f61157bf2e151b755f33b616418b0d8
Nischit <123927039+nischitgit@users.noreply.github.com>
2023-02-19 18:32:23 +0530

    Update simple_interest.c

--- simple_interest.c
+++ simple_interest.c
@@ -1,9 +1,13 @@
 // Simple Interest Calculation
+// Author - @nischit_ig (Instagram)
 #include <stdio.h>
 
 int
-main () {
+main() 
+{
+  // Variables
   int principal, rate, time, simple_interest;
+  // Instructions
   printf ("Simple Interest Calculation \n");
   printf ("Enter Principal Amount: ");
   scanf ("%d", &principal);
@@ -11,6 +15,7 @@ main () {
   scanf ("%d", &rate);
   printf ("Enter Time period: ");
   scanf ("%d", &time);
+  // Output
   simple_interest = (principal * rate * time) / 100;
   printf ("Principal Amount: %d \n", principal);
   printf ("Total Interest : %d \n", simple_interest);
